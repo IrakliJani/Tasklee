19e663fda3d2cf4e69e29455e0d6a3d659fee2bc
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-12-26 18:51:09 +0400

    added redux persistance

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 // @flow
 
+import { AsyncStorage } from 'react-native'
 import { createStore, combineReducers } from 'redux'
+import { persistStore, autoRehydrate, createTransform } from 'redux-persist'
+import { fromJS } from 'immutable'
 
 import tasksReducer from 'Tasklee/src/reducers/tasks'
 import settingsReducer from 'Tasklee/src/reducers/settings'
@@ -10,6 +13,16 @@ const rootReducer = combineReducers({
   settings: settingsReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, undefined, autoRehydrate())
+
+const immutableTransformer = createTransform(
+  (inbound) => inbound.toJS(),
+  (outbound) => fromJS(outbound)
+)
+
+persistStore(store, {
+  storage: AsyncStorage,
+  transforms: [immutableTransformer]
+})
 
 export default store
